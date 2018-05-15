6db1a9c85ab04f35c2e091004baf6e932495a68a
Fernando Erd <fernandoerd2012@gmail.com>
2018-05-15 17:18:40 -0300

    Change input

--- getTransferLeague.py
+++ getTransferLeague.py
@@ -6,7 +6,6 @@ import time
 headers = {'User-Agent': 
            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
-page = "https://www.transfermarkt.com/campeonato-brasileiro-serie-a/transfers/wettbewerb/BRA1/saison_id/2017"
 
 #################### Get Player Information ##################
 def removeWriteSpaces(text):
@@ -21,14 +20,8 @@ def getInformationTransfer(pageSoup, count, club):
 	position = pageSoup.find_all("td", {"class": "pos-transfer-cell"})
 	to = pageSoup.find_all("td", {"class": "no-border-links verein-flagge-transfer-cell"})
 	valueTransfer = pageSoup.find_all("td", {"class": "rechts"})
-
-#	print "Name" + str(len(player))
-#	print "Age" + str(len(age))
-#	print "Country" + str(len(country))
-#	print "Position" + str(len(position))
-#	print "Value" + str(len(to))
-#	print "To" + str(len(valueTransfer))
-#	while (i < 0):
+#	i = 0
+#	while (i < 5):
 #		print "------------Player-------------"
 #		print "Name: " + removeWriteSpaces(player[i].text)
 #		print "Age: " + removeWriteSpaces(age[i].text)
@@ -36,6 +29,7 @@ def getInformationTransfer(pageSoup, count, club):
 #		print "Position: " + removeWriteSpaces(position[i].text)
 #		print "ValueMarket: " + removeWriteSpaces(valueTransfer[2*i].text)
 #		print "to: " + removeWriteSpaces(to[i].text)
+#		print "Club Country: " + removeWriteSpaces(to[i].findChildren('img')[0]['alt'])
 #		print "ValueTransfer: " + removeWriteSpaces(valueTransfer[2*i + 1].text)
 #		print "\n"
 #		i = i + 1
@@ -52,15 +46,17 @@ def getInformationTransfer(pageSoup, count, club):
 			
 			if count == 1:
 				file.write(removeWriteSpaces(club) + ";")
+				file.write(leagueCountry + ";")
 				file.write(removeWriteSpaces(to[i].text) + ";")
+				file.write(removeWriteSpaces(to[i].findChildren('img')[0]['alt']) + ";")
 			else:
 				file.write(removeWriteSpaces(to[i].text) + ";")
+				file.write(removeWriteSpaces(to[i].findChildren('img')[0]['alt']) + ";")
 				file.write(removeWriteSpaces(club) + ";")
+				file.write(leagueCountry + ";")
 			
 			file.write(removeWriteSpaces(valueTransfer[2*i + 1].text) + "\n")
 			i = i + 1
-			print i
-
 
 ## init in table[4]
 def getTable_and_Club(pageSoup):
@@ -80,9 +76,13 @@ def getTable_and_Club(pageSoup):
 		except:
 			pass
 
+#################### Read Input #############################
+page = raw_input ("Enter by link: ")
+leagueCountry =  raw_input ("Enter by league country: ")
+
 ############################## Main #########################
 with open("teste.csv", "a") as file:
-	file.write("Player;Age;Country;Position;MarketValue;De;Para;TransferValue\n")
+	file.write("Player;Age;Country;Position;MarketValue;To;CountryClubTo;From;CountryClubFrom;TransferValue\n")
 pageTree = requests.get(page, headers=headers)
 pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
 getTable_and_Club(pageSoup)
